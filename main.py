3d0ac54e4e637b6ae6c3fb66dabae93a650e73d7
Hardik Jodhani <151493541+HARDIKJODHANI27@users.noreply.github.com>
2026-08-12 18:01:17 +0530

    Day 01: Initialize Datanest project

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Datanest",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("Datanest")
+
+st.subheader("Your data. Your insights.")
+
+st.write(
+    "An intelligent business analytics platform "
+    "for importing, analyzing, visualizing, and understanding data."
+)
