4476f52727d80c1185758f102ae154266a03797d
Hardik Jodhani <151493541+HARDIKJODHANI27@users.noreply.github.com>
2026-08-17 18:43:26 +0530

    Modified main.py to make the frontend of the project

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-import streamlit as st
+from app.frontend.app import *
 
-st.set_page_config(
-    page_title="Datanest",
-    page_icon="📊",
-    layout="wide"
-)
-
-st.title("Datanest")
-
-st.subheader("Your data. Your insights.")
-
-st.write(
-    "An intelligent business analytics platform "
-    "for importing, analyzing, visualizing, and understanding data."
-)
+run_app()
