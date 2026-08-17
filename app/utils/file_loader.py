0b20dd597804451d164bb282a51d3516f99483d0
Hardik Jodhani <151493541+HARDIKJODHANI27@users.noreply.github.com>
2026-08-17 18:43:04 +0530

    Modified file_koader.py to make the frontend of the project

--- app/utils/file_loader.py
+++ app/utils/file_loader.py
@@ -2,18 +2,22 @@ import pandas as pd
 from pathlib import Path
 
 
-def load_file(file_path):
-    file_path = Path(file_path)
+def load_file(file):
+    if isinstance(file, (str, Path)):
+        file_path = Path(file)
 
-    if not file_path.exists():
-        raise FileNotFoundError(f"File not found: {file_path}")
+        if not file_path.exists():
+            raise FileNotFoundError(f"File not found: {file_path}")
 
-    extension = file_path.suffix.lower()
+        extension = file_path.suffix.lower()
+
+    else:
+        extension = Path(file.name).suffix.lower()
 
     if extension == ".csv":
-        return pd.read_csv(file_path)
+        return pd.read_csv(file)
 
     if extension in [".xlsx", ".xls"]:
-        return pd.read_excel(file_path)
+        return pd.read_excel(file)
 
     raise ValueError(f"Unsupported file type: {extension}")
